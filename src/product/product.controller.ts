fb936f0dbbb74cef6922e982c09c2a86bd33296e
imtiyazthink <imtiyaz.mohammed@thinkitive.com>
2021-10-26 13:53:02 +0530

    feat : added products crud implementation

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('products')
 export class ProductController {
+    constructor(private productService: ProductService) { }
 
     @Get()
-    all() {
-        return 'all products';
+    async all() {
+        return this.productService.all();
+    }
+
+    @Post()
+    async create(@Body('title') title: string, @Body('content') content: string) {
+        return this.productService.create({
+            title,
+            content
+        })
+    }
+
+    @Get(':id')
+    async getById(@Param('id') id: number) {
+        return this.productService.getById(id)
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: number,
+        @Body('title') title: string,
+        @Body('content') content: string) {
+        return this.productService.update(id, {
+            title, content
+        })
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number) {
+        return this.productService.delete(id);
     }
 }
